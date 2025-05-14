0c61a3c99b566b2d7fdd9e3be40fb961b0bf9ae9
Nishanleo <153121022+Nishanleo@users.noreply.github.com>
2025-05-14 22:17:54 +0530

    Add files via upload

---
+++ UI/banners/SetupBanners.js
@@ -0,0 +1,8 @@
+const setupBanners = {
+    helpBanner : 'https://cdn.discordapp.com/attachments/1345444297121599538/1372153842833166336/ezgif-79deea14dffc58.gif?ex=6825bd59&is=68246bd9&hm=480740f1fd3af4a5a53c5f93156abe762c83e77c5e8d877382737a9e771d96e5&',
+    verificationBanner : 'https://i.ibb.co/pjHDXzXx/static.png',
+    ticketBanner : 'https://i.ibb.co/h1DBsRnv/ticket.gif',
+    autovcBanner : 'https://i.ibb.co/mk6Tj1r/autovc.gif',
+};
+
+module.exports = setupBanners;
